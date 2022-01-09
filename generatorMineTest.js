354291bb9f755a7447de8e1c5ac1fc553e79e1d6
voltprinter <68755438+voltprinter@users.noreply.github.com>
2022-01-09 18:59:51 +0000

    Update generatorMineTest.js

--- generatorMineTest.js
+++ generatorMineTest.js
@@ -1,57 +1,54 @@
-/* jshint esversion: 6 */
-/* jslint browser: true */
-/* global $, jQuery, alert */
-{
+(($) => {
+  var currentCode = 0;
+
   function zeroPad(number, targetLength = 5) {
     return parseInt(number).toString().padStart(targetLength, '0');
   }
 
   function addCheckDigit(couponCode) {
-    let checkDigit = 0;
+    var checkDigit = 0;
 
-    for (const i in couponCode) {
+    for (var i in couponCode) {
       checkDigit += parseInt(couponCode[i]) * (9 - (i % 2) * 2);
     }
 
     return couponCode + checkDigit.toString().slice(-1);
   }
 
-  function openBarcode(master, title, subtitle, expiry, uniq, prefix = 47400) {
+  function openBarcode(master, title, subtitle, expiry, uniq, prefix = 47450) {
     currentCode = master;
 
     if (typeof uniq === 'undefined') {
       uniq = Math.floor(Math.random() * 100000);
     }
 
-    const couponCode = addCheckDigit(zeroPad(prefix) + zeroPad(uniq) + zeroPad(master, 4));
-  }
+    var couponCode = addCheckDigit(zeroPad(prefix) + zeroPad(uniq) + zeroPad(master, 4));
 
-  if (typeof title !== 'undefined') {
-    let couponLimits = '';
+    if (typeof title !== 'undefined') {
+      var couponLimits = "";
 
-    if (title.includes('%')) {
-      couponLimits = `Coupon is valid for ${title} a single receipt `
-                    + 'purchase of any in-stock or Special Order merchandise '
-                    + `only ${subtitle.replace('purchases ', '')}.`;
-    } else if (title.includes('$')) {
-      minPurchase = subtitle.replace(/^.*(\$[0-9,\.]+).*$/, '$1');
-      couponLimits = `Coupon is valid for ${title} any in-stock or `
-                    + `special order merchandise single receipt purchase of ${
-                      minPurchase} or more (calculated before taxes &amp; `
-                    + 'after applicable discounts).';
+      if (title.includes('%')) {
+        couponLimits = 'Coupon is valid for ' + title + ' a single receipt ' +
+                       'purchase of any in-stock or Special Order merchandise ' +
+                       'only ' + subtitle.replace('purchases ', '') + '.';
+      } else if (title.includes('$')) {
+        minPurchase = subtitle.replace(/^.*(\$[0-9,\.]+).*$/, '$1');
+        couponLimits = 'Coupon is valid for ' + title + ' any in-stock or ' +
+                       'special order merchandise single receipt purchase of ' +
+                       minPurchase + ' or more (calculated before taxes &amp; ' +
+                       'after applicable discounts).';
+      }
+
+      $('.promo-title').html(title);
+      $('.promo-subtitle').html(subtitle);
+      $('.promo-expiry').html(expiry);
+      $('.coupon-limits').html(couponLimits);
     }
 
-    $('.promo-title').html(title);
-    $('.promo-subtitle').html(subtitle);
-    $('.promo-expiry').html(expiry);
-    $('.coupon-limits').html(couponLimits);
+    $('.barcode-image').attr('src', 'https://api-bwipjs.metafloor.com/?bcid=code128&text=' + couponCode);
+    $('.coupon-code').val(couponCode);
+    $('#coupon-modal').modal({ show: true });
   }
-
-  $('.barcode-image').attr('src', `https://api-bwipjs.metafloor.com/?bcid=code128&text=${couponCode}`);
-  $('.coupon-code').val(couponCode);
-  $('#coupon-modal').modal({
-    show: true,
-  });
 
   function copyCoupon() {
     $('.coupon-code').select();
@@ -63,15 +60,15 @@
   }
 
   function buildCoupons() {
-    const template = $('#coupon-template').html();
+    var template = $('#coupon-template').html();
 
-    for (const coupon of couponData) {
-      const $coupon = $(template);
+    for (var coupon of couponData) {
+      var $coupon = $(template);
 
       $coupon.find('.coupon-name').html(coupon.name);
       $coupon.find('.coupon-expiry').html(getExpiryDate(coupon.data.expiry));
 
-      for (const tag in coupon.data) {
+      for (var tag in coupon.data) {
         $coupon.find('.get-coupon').data(tag, coupon.data[tag]);
 
         if (tag === 'mastercode') {
@@ -84,24 +81,24 @@
   }
 
   function getExpiryDate(month) {
-    const monthInt = new Date(Date.parse(`${month} 1`)).getMonth();
-    const expiryDate = new Date((new Date()).getFullYear(), monthInt + 1, 0);
+    var monthInt = new Date(Date.parse(month + ' 1')).getMonth();
+    var expiryDate = new Date((new Date()).getFullYear(), monthInt + 1, 0);
 
     return expiryDate.toLocaleString('en-US', {
       month: 'short',
       day: 'numeric',
-      year: 'numeric',
+      year: 'numeric'
     });
   }
 
   $(document).ready(() => {
     buildCoupons();
 
-    $('.get-coupon').click(function () {
-      const master = $(this).data('mastercode');
-      const title = $(this).data('title');
-      const subtitle = $(this).data('subtitle');
-      let expiry = $(this).data('expiry');
+    $('.get-coupon').click(function() {
+      var master = $(this).data('mastercode');
+      var title = $(this).data('title');
+      var subtitle = $(this).data('subtitle');
+      var expiry = $(this).data('expiry');
 
       expiry = getExpiryDate(expiry);
 
@@ -112,4 +109,4 @@
     $('.print-coupon').click(printCoupon);
     $('.new-coupon').click(() => openBarcode(currentCode));
   });
-}
+})(jQuery);
