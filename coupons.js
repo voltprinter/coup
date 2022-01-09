8b8b3ccc23992ef14d4b82b1520e90d3b008ae5d
voltprinter <68755438+voltprinter@users.noreply.github.com>
2022-01-09 15:23:13 +0000

    Add files via upload

---
+++ coupons.js
@@ -0,0 +1,29 @@
+var couponData = [
+  {
+    "name": "10% off",
+    "data": {
+      "mastercode": "1061",
+      "title": "10% off",
+      "subtitle": "purchases up to $5,000",
+      "expiry": "August"
+    }
+  },
+  {
+    "name": "$10 off $50",
+    "data": {
+      "mastercode": "5842",
+      "title": "$10 off",
+      "subtitle": "purchases over $50",
+      "expiry": "January"
+    }
+  },
+  {
+    "name": "$20 off $100",
+    "data": {
+      "mastercode": "1127",
+      "title": "$20 off",
+      "subtitle": "purchases over $100",
+      "expiry": "August"
+    }
+  }
+];
