461890dcdd5cf7c8c72c2f4e0fc5fc4dac78dfa5
voltprinter <68755438+voltprinter@users.noreply.github.com>
2022-01-09 20:39:03 +0000

    Update coupons.js

--- coupons.js
+++ coupons.js
@@ -11,7 +11,7 @@ var couponData = [
   {
     "name": "$10 off $50",
     "data": {
-      "mastercode": "5842",
+      "mastercode": "3089",
       "title": "$10 off",
       "subtitle": "purchases over $50",
       "expiry": "January"
@@ -20,7 +20,7 @@ var couponData = [
   {
     "name": "$20 off $100",
     "data": {
-      "mastercode": "9582",
+      "mastercode": "3089",
       "title": "$20 off",
       "subtitle": "purchases over $100",
       "expiry": "January"
